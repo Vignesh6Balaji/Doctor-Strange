264f345da68295c9f23c8bf5dfbc474ec70860ce
Vignesh Balaji <Vignesh Balaji@FFE-Vignesh-Balaji>
2024-02-22 17:50:16 +0530

    Strongest Avenger

---
+++ America/src/test/java/X_Men/Wolverine.java
@@ -0,0 +1,14 @@
+package X_Men;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Wolverine {
+
+	@Test
+	public void logan() {
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Hi Deadpool");
+		}
+	}
+}
